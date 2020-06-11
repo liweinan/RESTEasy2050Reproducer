efd924df2e07a1fcbf2b4a1089a512b8f0ee3027
阿男 <l.weinan@gmail.com>
2020-06-11 20:13:37 +0800

    update

--- src/main/java/io/weli/resteasy2505/FooResource.java
+++ src/main/java/io/weli/resteasy2505/FooResource.java
@@ -1,12 +1,28 @@
 package io.weli.resteasy2505;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 @Path("/foo")
 public class FooResource {
-   @GET
-   public String get() {
-      return "Hello, foo!";
-   }
+    @GET
+    @Path("/dummy")
+    public String dummy() {
+        throw new WebApplicationException("-=-=-=-dummy-=-=-=-");
+    }
+
+    @GET
+    public String get() {
+        return "Hello, foo!";
+    }
+
+    @POST
+    @Path("boom")
+    public Response boom() {
+        return Response.serverError().entity(null).build();
+    }
+
 }
